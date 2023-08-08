012d117a7cb1eecf04677bf386345f54ff0f7c45
Tim Triemstra <root@timtr.com>
2023-08-07 20:46:49 -0700

    New implementations for Swift

--- fizzBuzz.swift
+++ fizzBuzz.swift
@@ -1,47 +1,66 @@
-import Foundation
+/** Simple implementation(s) of FizzBuzz
 
-/// this is a doc comment using a bold font
-/// - returns: hello
-/// - keyword: this is some text
-/// - experiment: to suggest ways to interact
-/// - note: an additional note within the prose
-/// - parameter: hello
-/// - important: for cautions and warnings
+This program will loop (1-100 in this case) and for numbers divisible by 3 print “Fizz”. For numbers divisible by 5 print “Buzz”. For numbers divisible by both 3 and 5 print “FizzBuzz”. Otherwise, just print out the iteration number.
+**/
 
-/* 
-    regular (not a doc) comment is less bold
-*/
-
-/** this is also a doc comment (see it is bold)
-
-- keyword: not working yet (not highlighted as shown above)
-- returns: not yet working either
-- note:  nope
-*/
+struct Colors {
+    static let reset = "\u{001B}[0;0m"
+    static let yellow = "\u{001B}[0;33m"
+    static let cyan = "\u{001B}[0;36m"
+}
 
 
-/// -returns: whether the number should be number, fizz, buzz, or both
-func fizzBuzz(_ num: Int) -> String {
-    switch num {
-        case _ where num % 15 == 0:
-            return "FizzBuzz"
-        case _ where num % 3 == 0:
-            return "Fizz"
-        case _ where num % 5 == 0:
-            return "Buzz"
+/// This is very simple comparison, no special case for 15
+///
+/// - Parameter num: the specific number to analyze
+/// - Returns: whether the number should be number, fizz, buzz, or both
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+func fizzBuzz1(_ checkNum: Int) -> String {
+    var answer = ""
+
+    if (checkNum % 3 == 0) { 
+        answer.append(Colors.yellow + "Fizz") }
+
+    if (checkNum % 5 == 0) {
+        answer.append(Colors.cyan + "Buzz") }
+        
+    if answer.isEmpty { 
+        answer = Colors.reset + "(\(checkNum.description))" }
+    
+    return answer
+}
+
+
+
+/// This is a switch statement using 15 - only one answer per option
+///
+/// - Parameter num: the specific number to analyze
+/// - Returns: whether the number should be number, fizz, buzz, or both
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+func fizzBuzz2(_ checkNum: Int) -> String {
+    switch checkNum {
+        case _ where checkNum % 15 == 0:
+            return "FizzBuzz (\(checkNum.description))"
+        case _ where checkNum % 3 == 0:
+            return "Fizz (\(checkNum.description))"
+        case _ where checkNum % 5 == 0:
+            return "Buzz (\(checkNum.description))"
         default:
-            return num.description
+            return checkNum.description
     }
 }
 
-func main() {
-    var result = "Start"
-    for num in 0...100 {
-        result += "\(fizzBuzz(num))\n"
-    }
 
-    print("\(result)")
+// A simple Swift program has the main run loop as top-level code, no "main"
+var result = "Starting... \n"
+for count in 1...100 {
+    print ("\(fizzBuzz1(count))")
 }
+//print("\(result)")
+
+
+// end of file.
+
 
 
 
